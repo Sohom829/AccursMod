54ff5947bb76995291d8d102efbb165f1eae8669
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-06-28 11:07:58 +0600

    Adding "Emerald Tools and Armors"
    This will emerald armor and tools

---
+++ src/main/java/net/mcreator/accurs/item/EmeraldToolsHoeItem.java
@@ -0,0 +1,40 @@
+
+package net.mcreator.accurs.item;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.HoeItem;
+import net.minecraft.world.item.CreativeModeTab;
+
+public class EmeraldToolsHoeItem extends HoeItem {
+	public EmeraldToolsHoeItem() {
+		super(new Tier() {
+			public int getUses() {
+				return 2380;
+			}
+
+			public float getSpeed() {
+				return 16f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 8f;
+			}
+
+			public int getLevel() {
+				return 10;
+			}
+
+			public int getEnchantmentValue() {
+				return 70;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(Items.EMERALD));
+			}
+		}, 0, -3f, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS));
+	}
+}
