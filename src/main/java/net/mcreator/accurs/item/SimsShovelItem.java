42a41ff6c71ba470998ed0c4dcbe9a0e6bee0752
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-07-01 22:49:04 +0600

    Adding "Sims" to game
    Sims is a new type ore pack which is ingot based so you have to smelt the ore. Its cyan looking ore

---
+++ src/main/java/net/mcreator/accurs/item/SimsShovelItem.java
@@ -0,0 +1,41 @@
+
+package net.mcreator.accurs.item;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+
+import net.mcreator.accurs.init.AccursModItems;
+
+public class SimsShovelItem extends ShovelItem {
+	public SimsShovelItem() {
+		super(new Tier() {
+			public int getUses() {
+				return 2380;
+			}
+
+			public float getSpeed() {
+				return 16f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 8f;
+			}
+
+			public int getLevel() {
+				return 10;
+			}
+
+			public int getEnchantmentValue() {
+				return 70;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(AccursModItems.SIMS_INGOT.get()));
+			}
+		}, 1, -3f, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS));
+	}
+}
